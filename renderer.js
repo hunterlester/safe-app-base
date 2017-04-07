4a6f73132a5c914b8f45487f36a298494dfd43d1
hunterlester <guilfordhunter@gmail.com>
2017-04-07 16:34:43 -0700

    save progress on openURL

--- renderer.js
+++ renderer.js
@@ -1,3 +1,38 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+let shell = require('electron').shell;
+let safeApp = require('safe-app');
+
+const appInfo = {
+	'id': 'net.safe.upload.mock',
+	'name': 'Host Website',
+	'vendor': 'GiveGame'
+}
+
+const containers = {
+  _public: [
+    'Read',
+    'Insert',
+    'Update',
+    'Delete',
+    'ManagePermissions'
+  ],
+  _publicNames: [
+    'Read',
+    'Insert',
+    'Update',
+    'Delete',
+    'ManagePermissions'
+  ]
+};
+
+const parseUrl = (url) => (
+  (url.indexOf('safe-auth://') === -1) ? url.replace('safe-auth:', 'safe-auth://') : url
+);
+
+setTimeout(() => {
+  safeApp.initializeApp(appInfo).then(app => app.auth.genAuthUri(containers, {own_container: true}).then(uri => {
+    shell.openExternal(parseUrl(uri.uri))
+  }))
+}, 3000)
